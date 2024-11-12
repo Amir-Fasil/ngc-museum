b67d5783973c5e07e14efae3e9811af5da8b178a
Amir-Fasil <136108634+Amir-Fasil@users.noreply.github.com>
2024-11-12 14:02:42 +0300

    Update sim.sh

--- exhibits/pc_discrim/sim.sh
+++ exhibits/pc_discrim/sim.sh
@@ -2,11 +2,11 @@
 ################################################################################
 # Simulate the PCN on the MNIST database
 ################################################################################
-DATA_DIR="../../data/mnist"
+DATA_DIR="../../data"
 
 rm -r exp/* ## clear out experimental directory
-python train_pcn.py  --dataX="$DATA_DIR/trainX.npy" \
-                     --dataY="$DATA_DIR/trainY.npy" \
-                     --devX="$DATA_DIR/validX.npy" \
-                     --devY="$DATA_DIR/validY.npy" \
+python train_pcn.py  --dataX="$DATA_DIR/AG_trainX_compressed.npz" \
+                     --dataY="$DATA_DIR/AG_trainY_compressed.npz" \
+                     --devX="$DATA_DIR/AG_testX_compressed.npz" \
+                     --devY="$DATA_DIR/AG_testY_compressed.npz" \
                      --verbosity=0
