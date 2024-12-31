578623068710661e3c8d972b2698837c88ff7e50
Amir-Fasil <136108634+Amir-Fasil@users.noreply.github.com>
2024-12-31 12:45:02 +0300

    Update train_bfasnn.py

--- exhibits/bfa_snn/train_bfasnn.py
+++ exhibits/bfa_snn/train_bfasnn.py
@@ -50,6 +50,15 @@ _X = jnp.load(dataX)
 _Y = jnp.load(dataY)
 Xdev = jnp.load(devX)
 Ydev = jnp.load(devY)
+
+_X = (_X - np.mean(_X, axis=0)) / np.std(_X, axis=0)
+_Y = (_Y - np.mean(_Y, axis=0)) / np.std(_Y, axis=0)
+Xdev = (Xdev - np.mean(Xdev, axis=0)) / np.std(Xdev, axis=0)
+Ydev = (Ydev - np.mean(Ydev, axis=0)) / np.std(Ydev, axis=0)
+
+
+
+
 x_dim = _X.shape[1]
 patch_shape = (int(jnp.sqrt(x_dim)), int(jnp.sqrt(x_dim)))
 y_dim = _Y.shape[1]
